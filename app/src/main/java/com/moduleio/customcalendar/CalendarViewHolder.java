c0628d8856f49964f0d5291066492ca07bd22d70
Wiscarlens Lucius <wiscarlens@gmail.com>
2024-03-23 16:52:24 -0400

    Add a background on select day

--- app/src/main/java/com/moduleio/customcalendar/CalendarViewHolder.java
+++ app/src/main/java/com/moduleio/customcalendar/CalendarViewHolder.java
@@ -1,5 +1,6 @@
 package com.moduleio.customcalendar;
 
+import android.graphics.Color;
 import android.view.View;
 import android.widget.TextView;
 
@@ -9,11 +10,15 @@ import androidx.recyclerview.widget.RecyclerView;
 public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener
 {
     public final TextView dayOfMonth;
+    private final View backgroundCircle;
     private final CalendarAdapter.OnItemListener onItemListener;
+    private static View previousView = null;
+
     public CalendarViewHolder(@NonNull View itemView, CalendarAdapter.OnItemListener onItemListener)
     {
         super(itemView);
         dayOfMonth = itemView.findViewById(R.id.cellDayText);
+        backgroundCircle = itemView.findViewById(R.id.backgroundCircle);
         this.onItemListener = onItemListener;
         itemView.setOnClickListener(this);
     }
@@ -21,6 +26,13 @@ public class CalendarViewHolder extends RecyclerView.ViewHolder implements View.
     @Override
     public void onClick(View view)
     {
+        if(previousView != null){
+            previousView.findViewById(R.id.backgroundCircle).setVisibility(View.INVISIBLE);
+            ((TextView) previousView.findViewById(R.id.cellDayText)).setTextColor(Color.BLACK);
+        }
+        backgroundCircle.setVisibility(View.VISIBLE);
+        dayOfMonth.setTextColor(Color.WHITE);
         onItemListener.onItemClick(getAdapterPosition(), (String) dayOfMonth.getText());
+        previousView = view;
     }
 }
